13e730f0af6d6821bc0db0829accef18d2ef6708
C80609A <c080609a@gmail.com>
2017-06-22 13:03:35 +0300

    develop: engine: добавил в `ActiveAdmin..load_paths` поддиректории `app/helpers`.

--- lib/c80_yax/engine.rb
+++ lib/c80_yax/engine.rb
@@ -5,6 +5,7 @@ module C80Yax
 
     initializer :c80_yax_engine do
       if defined?(ActiveAdmin)
+        ActiveAdmin.application.load_paths += Dir["#{config.root}/app/helpers/**/"]
         ActiveAdmin.application.load_paths += Dir["#{config.root}/app/models/**/"]
         #ActiveAdmin.application.load_paths += Dir["#{config.root}/app/models/concerns/**/"]
         ActiveAdmin.application.load_paths += Dir["#{config.root}/app/admin/c80_yax/**/"]
