487460eb5d18b89fc3f25fd1fd9c9c5d22a6740b
C80609A <c080609a@gmail.com>
2017-06-25 20:54:03 +0300

    feature_pack: engine Pack incoming.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in c80_yax.gemspec
 gemspec
+
+gem 'pack', path: 'engines/pack'
--- lib/c80_yax.rb
+++ lib/c80_yax.rb
@@ -4,6 +4,7 @@ require 'c80_yax/strsubcat_runtime_tables'
 require 'c80_yax/watermarker'
 require 'rails-observers'
 require 'mini_magick'
+require_relative '../engines/pack/lib/pack'
 
 module C80Yax
   def self.table_name_prefix
