7d7edbffd6c6afdfcf10512d65e5074279c30ac9
C80609A <c080609a@gmail.com>
2017-06-26 08:16:44 +0300

    feature_pack: js: init_select_picker переехал в отдельный файл.

--- app/assets/javascripts/backend/strsubcats.js
+++ app/assets/javascripts/backend/strsubcats.js
@@ -1,17 +1,5 @@
 "use strict";
 
-// при клике по habtm кнопке "добавить",
-// на всякий случай,
-// инициализируем в появившемся на экране блоке
-// возможный bootstrap select picker
-var init_select_picker = function (){
-    $('.has_many_add').on('click', function (){
-        setTimeout(function() {
-            $('.selectpicker').selectpicker();
-        }, 40);
-    });
-};
-
 var f_edit = function (){
     init_select_picker();
 };
