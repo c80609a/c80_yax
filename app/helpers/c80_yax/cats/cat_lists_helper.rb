92e9c4c38bbbcf0fc623e6d419802ae0c506b233
C80609A <c080609a@gmail.com>
2017-07-07 11:59:38 +0300

    master: вставил подменю с категориями в меню "Каталог".

--- app/helpers/c80_yax/cats/cat_lists_helper.rb
+++ app/helpers/c80_yax/cats/cat_lists_helper.rb
@@ -12,6 +12,39 @@ module C80Yax
                }
       end
 
+      def render_cats_strsubcats(&make_strsubcat_url)
+
+        rdo = Cat.includes(:strsubcats)
+        res = ''
+
+        rdo.each do |category|
+          r = "<h2 class='title'>#{category.title}</h2>"
+          ds = ul_strsubcats(category, &make_strsubcat_url)
+          next if ds.blank? # (**)
+          r += ds
+          res += "<li class='li_category' id='category_#{category.id}'>#{r}</li>"
+        end
+
+        "<ul class='ul_cats_strsubcats'>#{res}</ul>".html_safe
+
+      end
+
+      private
+
+      def ul_strsubcats(category, &make_url)
+        res = ''
+        # noinspection RubyResolve
+        category.strsubcats.each do |strsubcat|
+          id = "strsubcat_#{strsubcat.id}"
+          t = strsubcat.title
+          u = make_url.call(strsubcat)
+          d = "<a href='#{u}' title='#{t}'>#{t}</a>"
+          res += "<li class='li_strsubcat' id='#{id}'>#{d}</li>"
+        end
+        return res if res.blank? # (*)
+        "<ul class='ul_strsubcats'>#{res}</ul>"#.html_safe
+      end
+
     end
   end
 end
