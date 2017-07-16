f91e642a49d3b2c3cdf05029362441facdecaa36
C80609A <c080609a@gmail.com>
2017-07-16 17:23:11 +0300

    develop: добавил подменю в пункт "Акции" - вывожу туда список всех акций.

--- engines/of/app/helpers/of/offers_list_helper.rb
+++ engines/of/app/helpers/of/offers_list_helper.rb
@@ -16,5 +16,18 @@ module Of
 
     end
 
+    def render_ul_offers_list_flat(&make_offer_url)
+
+      p_make_offer_url = Proc.new &make_offer_url
+      offers = Offer.all
+
+      render :partial => 'of/offers_list_flat',
+             :locals => {
+                 offers: offers,
+                 p_make_offer_url: p_make_offer_url
+             }
+
+    end
+
   end
 end
