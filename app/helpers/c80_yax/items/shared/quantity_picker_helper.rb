ba6c7edc68500f74dab6de49adca4517232e22db
C80609A <c080609a@gmail.com>
2017-07-20 07:11:13 +0300

    master: item quantity picker: реализовал компонент.

---
+++ app/helpers/c80_yax/items/shared/quantity_picker_helper.rb
@@ -0,0 +1,16 @@
+module C80Yax
+  module Items
+    module Shared
+      module QuantityPickerHelper
+
+        def render_quantity_picker
+            render :partial => 'c80_yax/items/quantity_picker',
+                   :locals => {
+
+                   }
+        end
+
+      end
+    end
+  end
+end
