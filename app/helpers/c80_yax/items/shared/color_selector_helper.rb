d1bbed04f55771550c71cda26346dfcce2ec6c5a
C80609A <c080609a@gmail.com>
2017-07-20 04:08:05 +0300

    master: item color selector: заверстал компонент.

---
+++ app/helpers/c80_yax/items/shared/color_selector_helper.rb
@@ -0,0 +1,19 @@
+module C80Yax
+  module Items
+    module Shared
+      module ColorSelectorHelper
+
+        def render_color_selector(item)
+          colors = item.colors
+          unless colors.size.zero?
+            render :partial => 'c80_yax/items/color_selector',
+                   :locals => {
+                       colors: colors
+                   }
+          end
+        end
+
+      end
+    end
+  end
+end
