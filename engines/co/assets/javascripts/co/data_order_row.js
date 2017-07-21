4faa1a9c3d0b9d3949f47cafe2cb9a384cc6ad71
C80609A <c080609a@gmail.com>
2017-07-21 04:28:19 +0300

    develop: co: Лабаем js-класс `DataOrderRow`.

---
+++ engines/co/assets/javascripts/co/data_order_row.js
@@ -0,0 +1,15 @@
+var DataOrderRow = function (){
+    data = {
+        id: 0,
+        quantity: 0,
+        color: '#ffffff',
+        price: 0,
+        image_url: '',
+        item_url: '',
+        mark_order: true,
+        mark_metallic: false,
+        mark_super_colors: false
+    };
+
+    return data;
+};
